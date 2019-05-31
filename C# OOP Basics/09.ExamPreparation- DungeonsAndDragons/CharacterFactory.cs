67e3fda0229a81a7cfedc79e751c0d5a0779a3a1
borisbotev <boris.botev97@protonmail.com>
2019-05-31 03:30:36 +0300

    Update tasks

---
+++ C# OOP Basics/09.ExamPreparation- DungeonsAndDragons/CharacterFactory.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+public class CharacterFactory
+{
+    
+    public static Character CreateCharacter(string factionType, string characterType, string characterName)
+    {
+        var faction = FactionFactory.CreateFaction(factionType);
+
+        var character = characterType;
+
+        var name = characterName;
+
+        if (characterType == "Warrior")
+        {
+            return new Warrior(name, faction);
+        }
+        else if (characterType == "Cleric")
+        {
+            return new Cleric(name, faction);
+        }
+        throw new ArgumentException($"Invalid character \"{characterType}\"!");
+    }
+}
